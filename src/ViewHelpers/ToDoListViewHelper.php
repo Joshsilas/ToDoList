8a5541bf2c97a62a6461c9b7efb2185861ccd362
iO Academy <thejoshsilasbryan@gmail.com>
2023-09-22 14:59:19 +0100

    Css complete

--- src/ViewHelpers/ToDoListViewHelper.php
+++ src/ViewHelpers/ToDoListViewHelper.php
@@ -9,7 +9,7 @@ class ToDoListViewHelper
         $output = '';
         foreach ($tasks as $task) {
             $output .=
-                '<div class="form-check">
+                '<div class="form-check container text center">
                     <form method="POST" action="/completeTask">
                     <input type="hidden" value="'. $task->getId() . '" name="id">
                         <input class="btn btn-success mt-1" type="submit" value="Complete?" >
--- templates/index.php
+++ templates/index.php
@@ -9,20 +9,18 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
 </head>
 <body>
-<div class="container center">
-    <div class="row align-items-start">
-        <div class="col">
-    <h1 class="container text center">The To Do List</h1>
-    <form method="POST" action="/addTask" class="input-group w-25">
-        <input type="text" name="task" class="form-control">
-        <button type="submit" class="btn btn-outline-secondary">Add</button>
-    </form>
-            <?php echo \App\ViewHelpers\ToDoListViewHelper::displayToDoInAList($tasks) ?>
+<div class="WholeThing">
+    <div class="container text center col-6 justify-content-md">
+        <h1 class="container text center">The To Do List</h1>
+        <form method="POST" action="/addTask" class="input-group">
+            <input type="text" name="task" class="form-control">
+            <button type="submit" class="btn btn-success mt-1">Add</button>
+        </form>
+                <?php echo \App\ViewHelpers\ToDoListViewHelper::displayToDoInAList($tasks) ?>
+                <a href="showCompletedTask">
+                    <button class="btn btn-info d-flex justify-content-md"">See Completed</button>
+                </a>
     </div>
-        <div>
-            <a href="showCompletedTask">
-                <button class="btn btn-info">See Completed</button>
-            </a>
-        </div>
+</div>
 </body>
 </html>
